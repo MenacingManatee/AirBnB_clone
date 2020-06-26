1ad6dce5026a9aa828734c50f7541cc3403d16ec
Gabriel <gabrielabdul1@gmail.com>
2020-06-26 20:56:55 +0000

    pep8 validated

--- console.py
+++ console.py
@@ -4,10 +4,10 @@ import cmd
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
-from models.city import City 
-from models.amenity import Amenity 
-from models.place import Place 
-from models.review import Review 
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
@@ -33,9 +33,9 @@ class HBNBCommand(cmd.Cmd):
             return
         if args in self.class_names:
             class_dict = {'User': User(), 'State': State(),
-                        'City': City(), 'Amenity': Amenity(),
-                        'Place': Place(), 'Review': Review(),
-                        'BaseModel': BaseModel()}
+                          'City': City(), 'Amenity': Amenity(),
+                          'Place': Place(), 'Review': Review(),
+                          'BaseModel': BaseModel()}
             instance = class_dict[args]
             instance.save()
             print(instance.id)

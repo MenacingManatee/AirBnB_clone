8c9230d6eee9e4b1ea5f9ae5d5307a735da39f39
Brett Davis <brett.p.davis101@gmail.com>
2020-07-01 09:46:52 +0000

    Output fix for task 5: Reloaded datetime needs to be datetime.datetime format according to example

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,6 +32,12 @@ Public instance methods:
             c = d.copy()
             if '__class__' in d:
                 c.pop('__class__')
+            form = '%Y-%m-%dT%H:%M:%S.%f'
+            from datetime import datetime
+            if 'created_at' in c:
+                c['created_at'] = datetime.strptime(c['created_at'], form)
+            if 'updated_at' in c:
+                c['updated_at'] = datetime.strptime(c['updated_at'], form)
             ret.update({s: c})
         return ret
 

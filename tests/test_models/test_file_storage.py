bdbef01daebb61f4ca96baaf9e7e80556d70ffb9
Brett Davis <brett.p.davis101@gmail.com>
2020-07-02 01:12:55 +0000

    Finalizes file storage tests

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -155,3 +155,98 @@ class Test_File_Storage(unittest.TestCase):
             self.assertTrue(obj['description'] == 'generic description')
             self.assertTrue(obj['number_rooms'] == 9)
             f.close()
+
+    def test_review(self):
+        '''Tests for the review object saving to file'''
+        with open('file.json', "r") as f:
+            import json
+            dic = json.loads(f.read())
+            r_id = "Review.{}".format(self.r.id)
+            obj = dic[r_id]
+            self.assertTrue(r_id in dic)
+            self.assertTrue(obj['id'] == self.r.id)
+            try:
+                # This also tests to ensure to_dict() exists
+                r_dict = self.r.to_dict()
+            except Exception:
+                return False
+            self.assertTrue(obj['created_at'] == r_dict['created_at'])
+            self.assertTrue(obj['updated_at'] == r_dict['updated_at'])
+            self.assertTrue(obj == self.r.to_dict())
+            self.assertTrue(hasattr(self.r, 'place_id'))
+            self.assertTrue(hasattr(self.r, 'user_id'))
+            self.assertTrue(hasattr(self.r, 'text'))
+            f.close()
+        self.r.place_id = self.p.id
+        self.r.user_id = self.u.id
+        self.r.save()
+        with open('file.json', "r") as f:
+            import json
+            dic = json.loads(f.read())
+            obj = dic["Review.{}".format(self.r.id)]
+            self.assertTrue(obj['place_id'] == self.p.id)
+            self.assertTrue(obj['user_id'] == self.u.id)
+            self.assertTrue(self.r.text == '')
+            f.close()
+
+    def test_user(self):
+        '''Tests for saving user object to file'''
+        with open('file.json', "r") as f:
+            import json
+            dic = json.loads(f.read())
+            u_id = "User.{}".format(self.u.id)
+            obj = dic[u_id]
+            self.assertTrue(u_id in dic)
+            self.assertTrue(obj['id'] == self.u.id)
+            try:
+                # This also tests to ensure to_dict() exists
+                u_dict = self.u.to_dict()
+            except Exception:
+                return False
+            self.assertTrue(obj['created_at'] == u_dict['created_at'])
+            self.assertTrue(obj['updated_at'] == u_dict['updated_at'])
+            self.assertTrue(obj == self.u.to_dict())
+            f.close()
+        self.assertTrue(hasattr(self.u, 'email'))
+        self.assertTrue(hasattr(self.u, 'password'))
+        self.assertTrue(hasattr(self.u, 'first_name'))
+        self.assertTrue(hasattr(self.u, 'last_name'))
+        self.u.first_name = "Bob"
+        self.u.last_name = "Parr"
+        self.u.save()
+        with open('file.json', "r") as f:
+            import json
+            dic = json.loads(f.read())
+            obj = dic["User.{}".format(self.u.id)]
+            self.assertTrue(obj['first_name'] == "Bob")
+            self.assertTrue(obj['last_name'] == "Parr")
+            f.close()
+
+    def test_state(self):
+        '''Tests for saving state objects to file'''
+        with open('file.json', "r") as f:
+            import json
+            dic = json.loads(f.read())
+            s_id = "State.{}".format(self.s.id)
+            obj = dic[s_id]
+            self.assertTrue(s_id in dic)
+            self.assertTrue(obj['id'] == self.s.id)
+            try:
+                # This also tests to ensure to_dict() exists
+                s_dict = self.s.to_dict()
+            except Exception:
+                return False
+            self.assertTrue(obj['created_at'] == s_dict['created_at'])
+            self.assertTrue(obj['updated_at'] == s_dict['updated_at'])
+            self.assertTrue(obj == self.s.to_dict())
+            f.close()
+        self.assertTrue(hasattr(self.s, 'name'))
+        self.assertTrue(self.s.name == '')
+        setattr(self.s, 'name', 'Oklahoma')
+        self.s.save()
+        with open('file.json', "r") as f:
+            import json
+            dic = json.loads(f.read())
+            obj = dic["State.{}".format(self.s.id)]
+            self.assertTrue(obj['name'] == 'Oklahoma')
+            f.close()
